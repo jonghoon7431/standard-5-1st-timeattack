a8a74d252f20b2d6dcc72dc590a0f8046d9024d3
jonghoon lee <jonghoon7431@gmail.com>
2024-05-20 15:53:07 +0900

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
+
 export default function App() {
+  const [calc, setCalc] = useState(0);
+
+  let num = 0;
+  const add = (e) => {
+    (prev) => prev + calc;
+    // setCalc(0);
+  };
+
+  const reset = () => {
+    setCalc(0);
+  };
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input
+          value={num}
+          onChange={(event) => {
+            setCalc(event.target.value);
+          }}
+        />{" "}
+        만큼을 <button onClick={add}>더할게요</button> <button>뺄게요</button>
+        <button onClick={reset}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{calc}</p>
       </div>
     </div>
   );
 }
+//못하겠습니다,,,,,,
